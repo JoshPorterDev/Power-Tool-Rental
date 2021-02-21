8a6623d3d4fa01fa3dc628ee054a31b1cbec2866
JoshPorterDev <porterjosh417@gmail.com>
2021-02-21 14:58:23 -0600

    added action bar logo

--- app/src/main/java/com/example/powertool/MainActivity.java
+++ app/src/main/java/com/example/powertool/MainActivity.java
@@ -24,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         // Save widgets to variables
         button = findViewById(R.id.button);
         radioGroup = findViewById((R.id.radioGroup));
